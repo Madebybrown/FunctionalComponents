dc32f3e12245c90553511875d7866fb3b750031a
Madebybrown <madebybrown@gmail.com>
2022-12-16 13:20:39 +0000

    Add Api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
+import ContentAPI from './components/ContentAPI';
 import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
-      <ContentHooks />
+      <ContentAPI />
+      {/* <ContentHooks /> */}
       {/* <Content /> */}
       <Sidebar />
     </div>
